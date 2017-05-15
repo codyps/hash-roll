93372ca75863dcb165f9dd5f12681582fa115123
Cody P Schafer <dev@codyps.com>
2017-05-15 10:42:48 -0400

    tests: fix qc_bup

--- tests/qc_bup.rs
+++ tests/qc_bup.rs
@@ -11,7 +11,7 @@ quickcheck! {
         let mut m2 = rollsum::Bup::default();
 
         let v1 = m1.find_chunk_edge(&xs);
-        let v2 = m2.find_chunk_edge(&xs).map(|x| x+1).unwrap_or(0);
+        let v2 = m2.find_chunk_edge(&xs).unwrap_or(0);
 
         v1 == v2
     }
@@ -41,9 +41,7 @@ quickcheck! {
     }
 }
 
-#[test]
-fn simple_eq_1() {
-    let x = [92, 6, 28, 35, 68, 82, 35, 71, 34, 19, 9, 45, 97, 17, 11, 6, 53, 39, 93, 49, 29, 17, 37, 6, 39];
+fn chk_a(x: &[u8]) {
     let m1 = hash_roll::Bup::default();
     let mut m2 = rollsum::Bup::default();
 
@@ -53,10 +51,8 @@ fn simple_eq_1() {
     assert_eq!(v1,v2);
 }
 
-#[test]
-fn simple_eq_1b() {
+fn chk_b(x: &[u8]) {
     use rollsum::Engine;
-    let x = [92, 6, 28, 35, 68, 82, 35, 71, 34, 19, 9, 45, 97, 17, 11, 6, 53, 39, 93, 49, 29, 17, 37, 6, 39];
     let mut m1 = hash_roll::bup::RollSum::default();
     let mut m2 = rollsum::Bup::default();
     let cm = (1<<rollsum::bup::CHUNK_BITS) - 1;
@@ -68,4 +64,25 @@ fn simple_eq_1b() {
         assert_eq!(m1.digest(), m2.digest());
         assert_eq!(m1.at_split(), (m2.digest() & cm) == cm);
     }
+}
+
+
+#[test]
+fn simple_eq_1() {
+    chk_a(&[92, 6, 28, 35, 68, 82, 35, 71, 34, 19, 9, 45, 97, 17, 11, 6, 53, 39, 93, 49, 29, 17, 37, 6, 39]);
+}
+
+#[test]
+fn simple_eq_1b() {
+    chk_b(&[92, 6, 28, 35, 68, 82, 35, 71, 34, 19, 9, 45, 97, 17, 11, 6, 53, 39, 93, 49, 29, 17, 37, 6, 39]);
+}
+
+#[test]
+fn simple_eq_2() {
+    chk_a(&[67, 3, 23, 73, 86, 64, 26, 25, 81, 53, 26, 82, 98, 86, 28]);
+}
+
+#[test]
+fn simple_eq_3() {
+    chk_a(&[40, 58, 57, 0, 16, 2, 32, 88, 0, 22, 23, 74, 90, 88, 95, 99, 86]);
 }
