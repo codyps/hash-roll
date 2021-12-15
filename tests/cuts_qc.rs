b3f36501af5ac4ab1e31f4b89e3ec30fd7c4eb95
Cody P Schafer <dev@codyps.com>
2021-12-14 22:03:31 -0500

    update cuts_qc a bit

--- tests/cuts_qc.rs
+++ tests/cuts_qc.rs
@@ -5,8 +5,13 @@
 //
 //  - simd vs non-simd algorithms
 
-use hash_roll::Chunk;
+use hash_roll::{Chunk, ChunkIncr, ToChunkIncr};
 use proptest::prelude::*;
+
+fn splits_incr<C: ToChunkIncr>(chunker: &C, buf: &[u8]) -> Vec<usize> {
+    let ci = chunker.to_chunk_incr();
+    ci.iter_slices_strict(buf).map(|x| x.len()).collect()
+}
 
 fn splits_fce<C: Chunk>(chunker: &C, buf: &[u8], buf_sizes: &[usize]) -> Vec<usize> {
     let mut splits = Vec::new();
@@ -64,38 +69,47 @@ fn splits_fce<C: Chunk>(chunker: &C, buf: &[u8], buf_sizes: &[usize]) -> Vec<usi
     splits
 }
 
+macro_rules! test_find_chunk_edge {
+    ($($name:expr, $fnname:ident : $chunker:expr;)*) => {
+        $(
+            proptest! {
+            #[test]
+            #[cfg(feature = $name)]
+            fn $fnname(
+                data in prop::collection::vec(0u8..=255u8, 0..1000),
+                buf_sizes_1 in prop::collection::vec(1usize..500, 1..1000),
+                buf_sizes_2 in prop::collection::vec(1usize..500, 1..1000))
+            {
+                let chunker = $chunker;
+                let s1 = splits_fce(&chunker, &data[..], &buf_sizes_1[..]);
+                let s2 = splits_fce(&chunker, &data[..], &buf_sizes_2[..]);
+                let s3 = splits_incr(&chunker, &data[..]);
+                assert_eq!(s1, s2);
+                assert_eq!(s1, s3);
+            }
+        }
+        )*
+    }
+}
+
+test_find_chunk_edge! {
+    "gzip", gzip_fce_self_consistent_with_varying_buf_size: hash_roll::gzip::GzipRsyncable::default();
+    "mii", mii_fce_self_consistent_with_varying_buf_size: hash_roll::mii::Mii::default();
+    "zpaq", zpaq_fce_self_consistent_with_varying_buf_size: hash_roll::zpaq::Zpaq::with_average_size_pow_2(13);
+    "pigz", pigz_fce_self_consistent_with_varying_buf_size: hash_roll::pigz::PigzRsyncable::default();
+    "bup", bup_fce_self_consistent_with_varying_buf_size: hash_roll::bup::RollSum::default();
+    "zstd", zstd_fce_self_consistent_with_varying_buf_size: hash_roll::zstd::Zstd::default();
+    "gear", gear_fce_self_consistent_with_varying_buf_size: hash_roll::gear::Gear32::default();
+    "fastcdc", fastcdc_fce_self_consistent_with_varying_buf_size: hash_roll::fastcdc::FastCdc::default();
+    "ram", ram_fce_self_consistent_with_varying_buf_size: hash_roll::ram::Ram::with_w(8192);
+}
+
 proptest! {
     #[test]
-    #[cfg(feature = "gzip")]
-    fn gzip_fce_self_consistent_with_varying_buf_size(
-        data in prop::collection::vec(0u8..=255u8, 0..10000),
-        buf_sizes_1 in prop::collection::vec(1usize..5000, 1..10000),
-        buf_sizes_2 in prop::collection::vec(1usize..5000, 1..10000))
-    {
-        let chunker = hash_roll::gzip::GzipRsyncable::default();
-        let s1 = splits_fce(&chunker, &data[..], &buf_sizes_1[..]);
-        let s2 = splits_fce(&chunker, &data[..], &buf_sizes_2[..]);
-        assert_eq!(s1, s2);
-    }
-
-    #[test]
-    #[cfg(feature = "mii")]
-    fn mii_fce_self_consistent_with_varying_buf_size(
-        data in prop::collection::vec(0u8..=255u8, 0..10000),
-        buf_sizes_1 in prop::collection::vec(1usize..5000, 1..10000),
-        buf_sizes_2 in prop::collection::vec(1usize..5000, 1..10000))
-    {
-        let chunker = hash_roll::mii::Mii::default();
-        let s1 = splits_fce(&chunker, &data[..], &buf_sizes_1[..]);
-        let s2 = splits_fce(&chunker, &data[..], &buf_sizes_2[..]);
-        assert_eq!(s1, s2);
-    }
-
-    #[test]
-    #[cfg(feature = "buzhash_big")]
+    #[cfg(all(feature = "buzhash", feature = "slow_tests"))]
     fn buzhash_fce_self_consistent_with_varying_buf_size(
         seed: u8,
-        data in prop::collection::vec(0u8..=255u8, 0..10000),
+        data in prop::collection::vec(0u8..=255u8, 0..1000),
         buf_sizes_1 in prop::collection::vec(1usize..5000, 1..10000),
         buf_sizes_2 in prop::collection::vec(1usize..5000, 1..10000))
     {
@@ -120,97 +134,9 @@ proptest! {
         );
         let s1 = splits_fce(&chunker, &data[..], &buf_sizes_1[..]);
         let s2 = splits_fce(&chunker, &data[..], &buf_sizes_2[..]);
+        let s3 = splits_incr(&chunker, &data[..]);
         assert_eq!(s1, s2);
+        assert_eq!(s1, s3);
     }
 
-    #[test]
-    #[cfg(feature = "zpaq")]
-    fn zpaq_fce_self_consistent_with_varying_buf_size(
-        data in prop::collection::vec(0u8..=255u8, 0..10000),
-        buf_sizes_1 in prop::collection::vec(1usize..5000, 1..10000),
-        buf_sizes_2 in prop::collection::vec(1usize..5000, 1..10000))
-    {
-        let chunker = hash_roll::zpaq::Zpaq::with_average_size_pow_2(13);
-        let s1 = splits_fce(&chunker, &data[..], &buf_sizes_1[..]);
-        let s2 = splits_fce(&chunker, &data[..], &buf_sizes_2[..]);
-        assert_eq!(s1, s2);
-    }
-
-    #[test]
-    #[cfg(feature = "pigz")]
-    fn pigz_fce_self_consistent_with_varying_buf_size(
-        data in prop::collection::vec(0u8..=255u8, 0..10000),
-        buf_sizes_1 in prop::collection::vec(1usize..5000, 1..10000),
-        buf_sizes_2 in prop::collection::vec(1usize..5000, 1..10000))
-    {
-        let chunker = hash_roll::pigz::PigzRsyncable::default();
-        let s1 = splits_fce(&chunker, &data[..], &buf_sizes_1[..]);
-        let s2 = splits_fce(&chunker, &data[..], &buf_sizes_2[..]);
-        assert_eq!(s1, s2);
-    }
-
-    #[test]
-    #[cfg(feature = "bup")]
-    fn bup_fce_self_consistent_with_varying_buf_size(
-        data in prop::collection::vec(0u8..=255u8, 0..10000),
-        buf_sizes_1 in prop::collection::vec(1usize..5000, 1..10000),
-        buf_sizes_2 in prop::collection::vec(1usize..5000, 1..10000))
-    {
-        let chunker = hash_roll::bup::RollSum::default();
-        let s1 = splits_fce(&chunker, &data[..], &buf_sizes_1[..]);
-        let s2 = splits_fce(&chunker, &data[..], &buf_sizes_2[..]);
-        assert_eq!(s1, s2);
-    }
-
-    #[test]
-    #[cfg(feature = "zstd")]
-    fn zstd_fce_self_consistent_with_varying_buf_size(
-        data in prop::collection::vec(0u8..=255u8, 0..100000),
-        buf_sizes_1 in prop::collection::vec(1usize..5000, 1..10000),
-        buf_sizes_2 in prop::collection::vec(1usize..5000, 1..10000))
-    {
-        let chunker = hash_roll::zstd::Zstd::default();
-        let s1 = splits_fce(&chunker, &data[..], &buf_sizes_1[..]);
-        let s2 = splits_fce(&chunker, &data[..], &buf_sizes_2[..]);
-        assert_eq!(s1, s2);
-    }
-
-    #[test]
-    #[cfg(feature = "gear")]
-    fn gear_fce_self_consistent_with_varying_buf_size(
-        data in prop::collection::vec(0u8..=255u8, 0..100000),
-        buf_sizes_1 in prop::collection::vec(1usize..5000, 1..10000),
-        buf_sizes_2 in prop::collection::vec(1usize..5000, 1..10000))
-    {
-        let chunker = hash_roll::gear::Gear32::default();
-        let s1 = splits_fce(&chunker, &data[..], &buf_sizes_1[..]);
-        let s2 = splits_fce(&chunker, &data[..], &buf_sizes_2[..]);
-        assert_eq!(s1, s2);
-    }
-
-    #[test]
-    #[cfg(feature = "fastcdc")]
-    fn fastcdc_fce_self_consistent_with_varying_buf_size(
-        data in prop::collection::vec(0u8..=255u8, 0..100000),
-        buf_sizes_1 in prop::collection::vec(1usize..5000, 1..10000),
-        buf_sizes_2 in prop::collection::vec(1usize..5000, 1..10000))
-    {
-        let chunker = hash_roll::fastcdc::FastCdc::default();
-        let s1 = splits_fce(&chunker, &data[..], &buf_sizes_1[..]);
-        let s2 = splits_fce(&chunker, &data[..], &buf_sizes_2[..]);
-        assert_eq!(s1, s2);
-    }
-
-    #[test]
-    #[cfg(feature = "ram")]
-    fn ram_fce_self_consistent_with_varying_buf_size(
-        data in prop::collection::vec(0u8..=255u8, 0..100000),
-        buf_sizes_1 in prop::collection::vec(1usize..5000, 1..10000),
-        buf_sizes_2 in prop::collection::vec(1usize..5000, 1..10000))
-    {
-        let chunker = hash_roll::ram::Ram::with_w(8192);
-        let s1 = splits_fce(&chunker, &data[..], &buf_sizes_1[..]);
-        let s2 = splits_fce(&chunker, &data[..], &buf_sizes_2[..]);
-        assert_eq!(s1, s2);
-    }
 }
